45912df7fae3c142b95570f23d13a5d36ed84b56
suyeon <paksy0428@gmail.com>
2025-05-18 23:30:12 +0900

    페이징 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,38 +1,42 @@
+const { validationResult } = require("express-validator");
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
+// 전체 도서 조회, 카테고리 별 조회, 신간 조회
 const allBooks = (req, res) => {
-  let { category_id } = req.query;
+  let { category_id, newBooks, limit, currentPage } = req.query;
 
-  if (category_id) {
-    // 카테고리별 도서 목록 조회
-    let sql = `SELECT * FROM books WHERE category_id = ?`;
+  let offset = limit * (currentPage - 1);
 
-    conn.query(sql, category_id, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
+  let sql = `SELECT * FROM books`;
+  let values = [];
 
-      if (results.length > 0) {
-        return res.status(StatusCodes.OK).json(results);
-      } else {
-        return res.status(StatusCodes.NOT_FOUND).end();
-      }
-    });
-  } else {
-    // 전체 도서 조회
-    let sql = `SELECT * FROM books`;
-
-    conn.query(sql, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-
-      return res.status(StatusCodes.OK).json(results);
-    });
+  //now() -> '2023-12-32'
+  if (category_id && newBooks) {
+    sql += ` WHERE category_id = ? AND pub_date BETWEEN DATE_SUB('2023-12-31', INTERVAL 1 MONTH) AND '2023-12-31'`;
+    values = [category_id];
+  } else if (category_id) {
+    sql += ` WHERE category_id = ?`;
+    values = [category_id];
+  } else if (newBooks) {
+    sql += ` WHERE pub_date BETWEEN DATE_SUB('2023-12-31', INTERVAL 1 MONTH) AND '2023-12-31'`;
   }
+
+  sql += ` LIMIT ?, ?`;
+  values.push(offset, parseInt(limit));
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).end();
+    }
+  });
 };
 
 const bookDetail = (req, res) => {
