641900cccc38caad112c4cea6a1fed0fb08359fc
suyeon <paksy0428@gmail.com>
2025-05-19 23:14:48 +0900

    liked 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -8,7 +8,7 @@ const allBooks = (req, res) => {
 
   let offset = limit * (currentPage - 1);
 
-  let sql = `SELECT * FROM books`;
+  let sql = `SELECT *, (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes FROM books`;
   let values = [];
 
   //now() -> '2023-12-32'
@@ -40,12 +40,19 @@ const allBooks = (req, res) => {
 };
 
 const bookDetail = (req, res) => {
-  let { id } = req.params;
-  id = parseInt(id);
+  let { user_id } = req.body;
+  let book_id = req.params.id;
 
-  let sql = `SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id = ?`;
+  let sql = `SELECT *, 
+    (SELECT EXISTS (SELECT * FROM likes WHERE user_id=? AND liked_book_id=?)) AS liked,
+    (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes
+    FROM books 
+    LEFT JOIN category 
+    ON books.category_id = category.category_id
+    WHERE books.id=?;`;
+  let values = [user_id, book_id, book_id];
 
-  conn.query(sql, id, (err, results) => {
+  conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
