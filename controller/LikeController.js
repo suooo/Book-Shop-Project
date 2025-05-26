2d6838753d3784eedf8ed870e748845674d3fbb5
suyeon <paksy0428@gmail.com>
2025-05-26 21:54:02 +0900

    좋아요 기능에 jwt 추가

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -1,12 +1,16 @@
 const conn = require("../mariadb");
+const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const addLike = (req, res) => {
-  const { id } = req.params; // liked_book_id
-  const { user_id } = req.body;
+  const { book_id } = req.params;
+
+  let authorization = ensureAuthorization(req);
 
   let sql = `INSERT INTO likes (user_id, liked_book_id) VALUES (?, ?);`;
-  let values = [user_id, id];
+  let values = [authorization.id, book_id];
 
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -19,11 +23,12 @@ const addLike = (req, res) => {
 };
 
 const removeLike = (req, res) => {
-  const { id } = req.params; // liked_book_id
-  const { user_id } = req.body;
+  const { book_id } = req.params;
+
+  let authorization = ensureAuthorization(req);
 
   let sql = `DELETE FROM likes WHERE user_id = ? AND liked_book_id = ?;`;
-  let values = [user_id, id];
+  let values = [authorization.id, book_id];
 
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -34,5 +39,11 @@ const removeLike = (req, res) => {
     return res.status(StatusCodes.OK).json(results);
   });
 };
+
+function ensureAuthorization(req) {
+  let receivedJwt = req.headers["authorization"];
+  let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+  return decodedJwt;
+}
 
 module.exports = { addLike, removeLike };
