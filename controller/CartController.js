b1f44a7d00e6caf8c3f27f3d1e2021f4f399893b
suyeon <paksy0428@gmail.com>
2025-05-27 00:44:16 +0900

    장바구니 기능에 jwt 추가

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,12 +1,16 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // 장바구니 담기
 const addToCart = (req, res) => {
-  const { book_id, quantity, user_id } = req.body;
+  const { book_id, quantity } = req.body;
+  let authorization = ensureAuthorization(req, res);
 
   let sql = `INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?);`;
-  let values = [book_id, quantity, user_id];
+  let values = [book_id, quantity, authorization.id];
 
   conn.query(sql, values, (err, results) => {
     if (err) {
@@ -20,32 +24,43 @@ const addToCart = (req, res) => {
 
 // 장바구니 아이템 목록 조회
 const getCartItem = (req, res) => {
-  const { user_id, selected } = req.body;
+  const { selected } = req.body;
+  let authorization = ensureAuthorization(req, res);
 
-  let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price 
+  if (authorization instanceof jwt.TokenExpiredError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: "로그인 세션이 만료되었습니다. 다시 로그인 필요.",
+    });
+  } else if (authorization instanceof jwt.JsonWebTokenError) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: "잘못된 토큰입니다.",
+    });
+  } else {
+    let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price 
     FROM cartItems LEFT JOIN books 
     ON cartItems.book_id = books.id
     WHERE user_id = ?
     AND cartItems.id IN (?);`;
-  let values = [user_id, [...selected]];
+    let values = [authorization.id, [...selected]];
 
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
+    conn.query(sql, values, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
 
-    return res.status(StatusCodes.OK).json(results);
-  });
+      return res.status(StatusCodes.OK).json(results);
+    });
+  }
 };
 
 // 장바구니 아이템 삭제
 const removeCartItem = (req, res) => {
-  const { id } = req.params; // cartItem id
+  const { cartItemId } = req.params;
 
   let sql = `DELETE FROM cartItems WHERE id = ?;`;
 
-  conn.query(sql, id, (err, results) => {
+  conn.query(sql, [cartItemId], (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
@@ -54,5 +69,19 @@ const removeCartItem = (req, res) => {
     return res.status(StatusCodes.OK).json(results);
   });
 };
+
+function ensureAuthorization(req, res) {
+  try {
+    let receivedJwt = req.headers["authorization"];
+    let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+
+    return decodedJwt;
+  } catch (err) {
+    console.log(err.name);
+    console.log(err.message);
+
+    return err;
+  }
+}
 
 module.exports = { addToCart, getCartItem, removeCartItem };
