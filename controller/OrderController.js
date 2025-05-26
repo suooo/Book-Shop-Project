219dcd636ff65b9bb1a0b8f7817221d2837a7dbf
suyeon <paksy0428@gmail.com>
2025-05-26 17:25:57 +0900

    주문하기 기능 완성

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,35 +1,56 @@
-const conn = require("../mariadb");
+// const conn = require("../mariadb");
+const mariadb = require("mysql2/promise");
 const { StatusCodes } = require("http-status-codes");
 
-const order = (req, res) => {
+const order = async (req, res) => {
+  const conn = await mariadb.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    database: "Bookshop",
+    dateStrings: true,
+  });
+
   const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } =
     req.body;
 
-  let delivery_id;
-  let order_id;
-
+  // delivery 테이블 삽입
   let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?);`;
   let values = [delivery.address, delivery.receiver, delivery.contact];
-  delivery_id = results.insertId;
 
+  let [results] = await conn.execute(sql, values);
+  let delivery_id = results.insertId;
+
+  // orders 테이블 삽입
   sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?);`;
   values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-  order_id = results.insertId;
 
+  [results] = await conn.execute(sql, values);
+  let order_id = results.insertId;
+
+  // 장바구니에서 book_id와 quantity 조회
+  sql = `SELECT book_id, quantity FROM cartItems WHERE id IN (?);`;
+  let [orderItems, fields] = await conn.query(sql, [items]);
+
+  // orderedBook 테이블 삽입
   sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`;
   values = [];
-  items.forEach((item) => {
-    values.push([order_id, items.book_id, items.quantity]);
+  orderItems.forEach((item) => {
+    values.push([order_id, item.book_id, item.quantity]);
   });
 
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
+  [results] = await conn.query(sql, [values]);
 
-    return res.status(StatusCodes.OK).json(results);
-  });
+  // 장바구니에서 삭제
+  results = await deleteCartItems(conn, items);
+
+  return res.status(StatusCodes.OK).json(results);
+};
+
+const deleteCartItems = async (conn, items) => {
+  let sql = `DELETE FROM cartItems WHERE id IN (?); `;
+  let result = await conn.query(sql, [items]);
+  return result;
 };
 
 const getOrderList = (req, res) => {
